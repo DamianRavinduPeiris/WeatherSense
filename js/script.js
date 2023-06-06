b9c9f1c9f7deb901654db74d5149e7f5f180fe59
Damian Peiris <drpeiris3@gmail.com>
2023-06-06 15:28:53 +0530

    Current weather 1/2 - done.

--- js/script.js
+++ js/script.js
@@ -27,28 +27,14 @@ function fetchCurrentData(p1, p2) {
         var data = res.json();
         data.then((json) => {
             $(".weatherContainer").css("display", "flex");
-            $("#cityName").append("<h1>" + json.location.name + ".</h1>")
+            $("#cityName").append("<h1>" +"🗺️ Seems like you are in "+"<mark>"+json.location.name+"</mark>"+json.location.country+".</h1>")
+            $(".weatherDetails").append("<h1 class='commonText weatherText'>"+"Current Tempreature :   "+json.current.temp_c +"℃  |   "+  json.current.temp_f +"℉</h1><br><br>");
+            $(".weatherDetails").append("<img class='weatherIcon' src="+json.current.condition.icon+" ><br><br>");
+            $(".weatherDetails").append("<h1 class='commonText weatherText'>"+"Current Condition :   "+json.current.condition.text+"</h1><br><br>");
+            $(".weatherDetails").append("<h1 class='commonText weatherText'>Humidity : "+json.current.humidity+"</h1><br><br>")
+            fetchImage(json.location.country);
             /*Adding 500px to the current scroll position.*/
             $(window).scrollTop($(window).scrollTop() + 500);
-            let image = fetch("https://api.unsplash.com/search/photos?client_id=lz0WtbT_YAZdZKUvfjBLkO9Fifnhw6y9S4kYJx7cj0A&query=indias&order_by=relavent&page=9")
-            image.then((response)=>{
-                myJson = response.json();
-                myJson.then((finalResult)=>{
-                    var randomIndex = Math.floor(Math.random() * finalResult.results.length); // Generate a random index within the range of the array
-                    $("body").css("background-image","url("+finalResult.results[randomIndex].urls.full)+")";
-
-                });
-                myJson.catch((error)=>{
-                    showAlert("Error","Something went wrong! "+error,"error");
-
-                });
-            })
-            image.catch((error)=>{
-                showAlert("Error","Something went wrong! "+error,"error");
-
-            })
-
-
 
 
         });
@@ -78,6 +64,27 @@ function showAlert(title, msg, icon) {
         customClass: "commonText"
     });
 
+
+}
+
+function fetchImage(country){
+    let image = fetch("https://api.unsplash.com/search/photos?client_id=lz0WtbT_YAZdZKUvfjBLkO9Fifnhw6y9S4kYJx7cj0A&order_by=relavent&page=100&query="+country)
+    image.then((response)=>{
+        myJson = response.json();
+        myJson.then((finalResult)=>{
+            var randomIndex = Math.floor(Math.random() * finalResult.results.length); // Generate a random index within the range of the array
+            $("body").css("background-image","url("+finalResult.results[randomIndex].urls.full)+")";
+
+        });
+        myJson.catch((error)=>{
+            showAlert("Error","Something went wrong! "+error,"error");
+
+        });
+    })
+    image.catch((error)=>{
+        showAlert("Error","Something went wrong! "+error,"error");
+
+    })
 
 }
 
